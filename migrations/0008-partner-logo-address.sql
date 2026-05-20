4a5ca60cb70785ecaaf6ab8f5eb06f85cd4b06af
jmlapido <ijmlapido@gmail.com>
2026-05-20 18:37:25 +0400

    feat: add logo_key and address columns to partners table

---
+++ migrations/0008-partner-logo-address.sql
@@ -0,0 +1,4 @@
+ALTER TABLE partners ADD COLUMN logo_key        TEXT;
+ALTER TABLE partners ADD COLUMN address_street  TEXT;
+ALTER TABLE partners ADD COLUMN address_city    TEXT;
+ALTER TABLE partners ADD COLUMN address_country TEXT;
